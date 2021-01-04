74a7d80728b9c686951320666f97ce2c23d4ef67
xiaozhenzhen001 <xiaozhenzhen001@ke.com>
2021-01-04 11:27:21 +0800

    栈

--- src/first/Offer5从尾到头打印链表.java
+++ src/first/Offer5从尾到头打印链表.java
@@ -1,21 +1,26 @@
-package first;//输入一个链表的头结点，从尾到头打印出每个节点的值(从尾到头，栈的特点，考虑用栈)
+package first;
 
 import utils.NodeUtil;
 import utils.myObject.Node;
 
+import java.util.Objects;
 import java.util.Stack;
 
+
+/**
+ * 问题:输入一个链表的头结点，从尾到头打印出每个节点的值(从尾到头，栈的特点，考虑用栈)
+ */
 public class Offer5从尾到头打印链表 {
     public static void main(String[] args) {
         Node head = NodeUtil.createListWithHead();
-        System.out.print("利用栈，从尾到头访问：");
-        printListReversinglyIterratively(head);
+        System.out.print("利用栈特性,从尾到头访问: ");
+        printListReverselyWithStack(head);
         System.out.println();
-        System.out.print("利用递归，从尾到头访问：");
-        printListReversinglyRecursively(head);
+        System.out.print("利用递归,从尾到头访问: ");
+        printListReverselyRecursively(head);
     }
 
-    public static void printListReversinglyIterratively(Node head) {
+    public static void printListReverselyWithStack(Node head) {
         if (head == null || head.next == null)
             return;
         Node p = head.next;
@@ -29,22 +34,22 @@ public class Offer5从尾到头打印链表 {
 
     //遍历栈
     public static void traversalOfStack(Stack<Node> stack) {
-        if (stack == null)
+        if (Objects.isNull(stack))
             return;
         while (!stack.isEmpty())
             System.out.print(stack.pop().info + " ");
     }
 
-    public static void printListReversinglyRecursively(Node head) {
-        if (head == null || head.next == null)
+    public static void printListReverselyRecursively(Node head) {
+        if (Objects.isNull(head) || Objects.isNull(head.next))
             return;
-        printListReversinglyRecursivelyWithoutHead(head.next);
+        printListReverselyRecursivelyWithoutHead(head.next);
     }
 
-    public static void printListReversinglyRecursivelyWithoutHead(Node p) {
+    public static void printListReverselyRecursivelyWithoutHead(Node p) {
         if (p == null)
             return;
-        printListReversinglyRecursivelyWithoutHead(p.next);
+        printListReverselyRecursivelyWithoutHead(p.next);
         System.out.print(p.info + " ");
     }
 }
